d7490724fd2f7e79d1325b61c6e38757fe187427
mnchn <1113alscks@naver.com>
2025-10-26 16:58:00 +0900

    오류 두번 나오는거 없앰

--- Piece.cpp
+++ Piece.cpp
@@ -2,7 +2,7 @@
 
 bool Piece::MovePos(File x, Rank y, Cell(&board)[Rank::Ranksize][File::Filesize], Piece*& capturedPiece)
 {
-	if (!isMoveable(x, y, board)) { cout << "유효하지 않은 이동" << endl; system("pause"); return false; }
+	if (!isMoveable(x, y, board)) { system("pause"); return false; }
 
 	if(board[y][x].currentPiece != PieceType::typeNone)
 	{
